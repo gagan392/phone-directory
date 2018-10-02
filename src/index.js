e16c6ca9d6197e12543b90f4d5a6008b853599ee
Gagan Raj M <gagan.rajm@ellucian.com>
2018-10-03 02:21:36 +0530

    Added ‘AddSubscriber’ component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './common/common.css';
 import App from './App';
+import AddSubscriber from './AddSubscriber';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AddSubscriber />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
