5a4140b1ee151c5f2c4f71f61938685b9151b296
Gagan Raj M <gagan.rajm@ellucian.com>
2018-10-02 18:53:06 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-		  Phone Directory
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Fragment>
+				{/* Fragments return children without adding extra node in actual DOM
+						Shorthand: <>...</> */}
+				{/* <div className="header">Phone Directory</div>
+				<button>Add</button>
+				<div>
+					<span> Name: </span><br />
+					<span> Phone: </span>
+				</div> */}
+				<label htmlFor="name">Name: </label>
+				<input type="text" id="name" placeholder="Type Here" defaultValue="Gagan"/>
+			</Fragment>
+		);
+	}
 }
 
 export default App;
